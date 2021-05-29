3d89e13ada5316513e23894e618bbfce860a27d9
Urmzd <urmzds@gmail.com>
2021-05-29 18:31:05 -0400

    remove subdir

---
+++ frontend/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "neuro tracker",
+  },
+  plugins: [
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
