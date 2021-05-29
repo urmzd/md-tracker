7d9a060c610fbd95eed9a1113e418e380aabf070
Bryce Alexander MacInnis <Bryce.MacInnis@dal.ca>
2021-05-29 19:17:37 -0300

    Adds Symptom model to models

---
+++ back-end/models/symptom.rb
@@ -0,0 +1,14 @@
+class Symptom
+  def initialize(name, description)
+    @name = name
+    @description = description
+  end
+
+  def name
+    return @name
+  end
+
+  def description
+    return @description
+  end
+end
