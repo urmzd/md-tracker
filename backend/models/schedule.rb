5bc53b09af5be702990aac86d4a842e68a6fd643
Urmzd <urmzds@gmail.com>
2021-08-28 20:11:13 -0400

    Remove ruby project

--- backend/models/schedule.rb
+++
@@ -1,7 +0,0 @@
-class Schedule
-  def initialize(prescription, day, time)
-    @prescription = prescription
-    @day = day
-    @time = time
-  end
-end
--- backend/sql/create-table.sql
+++
@@ -1,8 +0,0 @@
-use neurotrackerdb;
-
-CREATE TABLE IF NOT EXISTS users (
-  phone_number VARCHAR(50) NOT NULL,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255);
-  PRIMARY KEY (phone_number)
-);
